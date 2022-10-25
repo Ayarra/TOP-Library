9e1da4d8ef07e87149f5f7f906eeac41e27363ac
ryanmccarthy031 <ryanmccarthy031@gmail.com>
2022-10-25 16:47:28 -0400

    A few changes

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-let myLibrary = [];
 let libraryDisplay = [];
 let globalID = 0;
+
+// I moved these up here to keep all the variables you're declaring outside of functions together.
+let table = document.querySelector("table");
+let bookForm = document.querySelector("form");
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -9,46 +12,78 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-let createReadBtn = (book) => {
-  let dataBook = document.createElement("td");
-  let statusBtn = document.createElement("button");
-  statusBtn.classList.add("status-bttn");
-  statusBtn.innerText = book.read;
-  statusBtn.addEventListener("click", (e) => {
-    if (statusBtn.innerText === "Read") statusBtn.innerText = "Not Read";
-    else statusBtn.innerText = "Read";
-    book.read = statusBtn.innerText;
-  });
-  dataBook.appendChild(statusBtn);
-  return dataBook;
+// The patterns for creating a read button and a delete button are very similar. The only differences
+// are the class, the text, and the event listener. By passing those into a single function, you can
+// reuse that code.
+let createButtonCell = (vals) => {
+  let cell = document.createElement("td");
+  let btn = document.createElement("button");
+
+  btn.classList.add(vals.class);
+  btn.innerText = vals.text;
+  btn.addEventListener("click", (e)=>vals.listener(e, btn))
+
+  cell.appendChild(btn);
+  vals.row.appendChild(cell)
+}
+
+let createReadBtn = (row, book) => {
+  // Create text options in a map, because a map will allow boolean keys.
+  // You could cheat this by using JavaScript's reading of 0 as falsy and 
+  // just use an object, but a map is a little more clear.
+  let txt = new Map()
+  txt.set(true, "Read")
+  txt.set(false, "Not Read")
+
+  let listener = (e, statusBtn) => {
+    book.read = !book.read;
+    // Here we use the map to get the appropriate text.
+    statusBtn.innerText = txt.get(book.read)
+  }
+
+  // This just creates an object with all the values we need to make a button:
+  // Class, text, listener, and the row that the button will be appended to.
+  let btn = {
+    class: "status-bttn",
+    text: txt.get(book.read),
+    listener,
+    row,
+  }
+
+  createButtonCell(btn);
 };
 
-let createDeleteBtn = () => {
-  let dataBook = document.createElement("td");
-  let deleteBtn = document.createElement("button");
-  deleteBtn.classList.add("delete-bttn");
-  deleteBtn.innerText = "Delete";
-  deleteBtn.addEventListener("click", (e) => {
-    let toDeleteBook = e.target.parentNode.parentNode;
-    let deleteId = toDeleteBook.getAttribute("data-id");
+// By passing the book's id into the function to create the button, we can avoid
+// selecting the parent node of a parent node or doing any other similar DOM traversal.
+// Instead, we can just search for the document with the id we're looking for.
+let createDeleteBtn = (row, id) => {
+  let listener = () => {
+    let toDeleteBook = document.querySelector(`[data-id="${id}"]`);
     toDeleteBook.remove();
     libraryDisplay = libraryDisplay.filter((row) => {
-      return row.getAttribute("data-id") != deleteId;
+      return row.getAttribute("data-id") != id;
     });
-  });
-  dataBook.appendChild(deleteBtn);
-  return dataBook;
+  }
+
+  // This just creates an object with all the values we need to make a button:
+  // Class, text, listener, and the row that the button will be appended to.
+  let btn = {
+    class: "delete-bttn",
+    text: "Delete",
+    listener,
+    row,
+  }
+
+  createButtonCell(btn);
 };
 
 function addBook(title, author, pages, read) {
   // Add Book to array of books
   let newBook = new Book(title, author, pages, read);
-  myLibrary.unshift(newBook);
 
   // Add Book to Row in Table
   const rowBook = document.createElement("tr");
   rowBook.setAttribute("data-id", globalID);
-  globalID++;
   for (data in newBook) {
     if (data != "read") {
       let dataBook = document.createElement("td");
@@ -58,41 +93,38 @@ function addBook(title, author, pages, read) {
     }
   }
 
-  let readStatus = createReadBtn(newBook);
-  rowBook.appendChild(readStatus);
-  let deleteBtn = createDeleteBtn();
-  rowBook.appendChild(deleteBtn);
+  // Passing in the row here allows us to handle the appending for both buttons in their functions
+  createReadBtn(rowBook, newBook);
+  // Passing in the global id here will let you simplify the function.
+  createDeleteBtn(rowBook, globalID);
+
   libraryDisplay.unshift(rowBook);
+
+  // Since we're using the global id a little later, the increment needs to be 
+  // relocated to the end of the function.
+  globalID++;
 
   return rowBook;
 }
 
-let table = document.querySelector("table");
-
-let displayBooks = (library) => {
-  for (let book in library) {
-    table.appendChild(library[book]);
-  }
-};
-
-let clearTable = (table) => {
-  let rows = table.getElementsByTagName("tr");
-  while (rows.length > 1) {
-    rows[1].parentNode.removeChild(rows[1]);
-  }
-};
-
-let bookForm = document.querySelector("form");
 
 bookForm.onsubmit = (e) => {
   e.preventDefault();
-  addBook(
+  let book = addBook(
     e.target.elements.title.value,
     e.target.elements.author.value,
     e.target.elements.pages.value,
-    e.target.elements.status.value
+    // Making this a boolean value will make managing text a little cleaner. You could make the 
+    // value of the element true or false rather than "Read" or "Not Read", but the value of the element
+    // will still be read as a string. This makes sure that you're working with a boolean value.
+    e.target.elements.status.value==="Read"
   );
-  clearTable(table);
-  displayBooks(libraryDisplay);
+
+  // Since you return the entire row element from addBook, you can just prepend it.
+  // There's no need to clear everything and refill the entire table with every added book.
+  // That might be a useful feature to keep in mind if you added the ability to drag rows to 
+  // sort them, however.
+  table.prepend(book)
+
   return false;
 };
